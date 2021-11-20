a5b42b1304dac1398aec2ac245b1f4b0d51efac2
allo0001 <77399306+allo0001@users.noreply.github.com>
2021-11-20 17:34:40 +0300

    2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'title': 'Магазин',
+    }
+    return render(request, 'mainapp/index.html', context)
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {
+        'title': 'Товары',
+    }
+    return render(request, 'mainapp/products.html', context)
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    context = {
+        'title': 'Контакты',
+    }
+    return render(request, 'mainapp/contact.html', context)
