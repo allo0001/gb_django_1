524b7768e25414b2e7b4d5622eb39a631fa07ac1
allo0001 <77399306+allo0001@users.noreply.github.com>
2021-11-20 16:50:46 +0300

    1

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 import mainapp.views as mainapp
 
 urlpatterns = [
-    path('', mainapp.index),
-    path('products/', mainapp.products),
-    path('contact/', mainapp.contact),
+    path('', mainapp.index, name='index'),
+    path('products/', mainapp.products, name='products'),
+    path('contact/', mainapp.contact, name='contact'),
 
     path('admin/', admin.site.urls),
 ]
