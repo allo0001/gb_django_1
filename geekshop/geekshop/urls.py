fdc61e3f901cc0887923c9f69b36ffc96c0c5b9b
allo0001 <77399306+allo0001@users.noreply.github.com>
2021-11-24 21:28:46 +0300

    3

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -5,6 +5,7 @@ import mainapp.views as mainapp
 urlpatterns = [
     path('', mainapp.index, name='index'),
     path('products/', mainapp.products, name='products'),
+    path('products/<name>/', mainapp.products, name='product_category'),
     path('contact/', mainapp.contact, name='contact'),
 
     path('admin/', admin.site.urls),
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render
 
+
+links_menu = [
+    {'href': 'home', 'name': 'Дом'},
+    {'href': 'modern', 'name': 'Модерн'},
+    {'href': 'office', 'name': 'Офис'},
+    {'href': 'classic', 'name': 'Классика'},
+]
+
+
 def index(request):
     context = {
         'title': 'Магазин',
+
     }
     return render(request, 'mainapp/index.html', context)
 
-def products(request):
+
+def products(request, name=None):
     context = {
         'title': 'товары',
+        'links_menu': links_menu,
+
     }
     return render(request, 'mainapp/products.html', context)
+
 
 def contact(request):
     context = {
