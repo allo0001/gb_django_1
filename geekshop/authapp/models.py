c5814241000809f57cd923bcd0e67fbc04d2880b
allo0001 <77399306+allo0001@users.noreply.github.com>
2021-12-02 21:34:58 +0300

    1

---
+++ geekshop/authapp/models.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class ShopUser(AbstractUser):
+    avatar = models.ImageField(upload_to='users', blank=True)
+    age = models.SmallIntegerField(default=18, verbose_name='Возпаст')
