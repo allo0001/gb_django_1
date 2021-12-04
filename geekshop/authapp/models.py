30b56db0850eaa861ae74c07c862921a42e343fc
allo0001 <77399306+allo0001@users.noreply.github.com>
2021-12-04 22:26:13 +0300

    fin

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users', blank=True)
-    age = models.SmallIntegerField(default=18, verbose_name='Возпаст')
+    age = models.SmallIntegerField(default=18, verbose_name='Возраст')
